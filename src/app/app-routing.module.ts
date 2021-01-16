83cf91012be0fca1dc08a704bd7b8a0260d52662
carlos <hernandez1989carlos@gmail.com>
2021-01-16 12:42:12 -0500

    [create] app/not-found-page : create notFoundPage components a modules/ [create] app/not-found-page/not-fount-page.module.ts : modularizate the component / [create] app/not-found-page/not-fount-page-routing.module.ts : add routes /[create] app/not-found-page/components : create notFountPage component with html,scss, ts files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,13 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./not-found-page/not-found-page.module').then(
+        (m) => m.NotFountPageModule
+      ),
+  },
 ];
 
 @NgModule({
